824a948ffc8d3c5daa2e19eddc4cbf60054d7b58
Hyblocker <hyblocker@protonmail.com>
2022-06-04 22:43:34 +0200

    Implement sidebar task control

--- Amethyst-Installer/Controls/SidebarTask.xaml.cs
+++ Amethyst-Installer/Controls/SidebarTask.xaml.cs
@@ -24,5 +24,57 @@ namespace amethyst_installer_gui.Controls
         {
             InitializeComponent();
         }
+
+        public SiderbarTaskState State
+        {
+            get { return (SiderbarTaskState)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("State", typeof(SiderbarTaskState), typeof(SidebarTask), new PropertyMetadata(SiderbarTaskState.Default, new PropertyChangedCallback(StateChanged)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(SidebarTask), new PropertyMetadata("Task", new PropertyChangedCallback(TextChanged)));
+
+
+        private static void StateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            string stateString = "Default";
+            switch ( (SiderbarTaskState)e.NewValue )
+            {
+                case SiderbarTaskState.Default:
+                    stateString = "Default";
+                    break;
+                case SiderbarTaskState.Checkmark:
+                    stateString = "Checkmark";
+                    break;
+                case SiderbarTaskState.Question:
+                    stateString = "Question";
+                    break;
+
+            }
+
+            Console.WriteLine($"/Resources/Icons/4x/{stateString}.png");
+            (d as SidebarTask).taskStateIcon.Source = new BitmapImage(new Uri($"/Resources/Icons/4x/{stateString}.png", UriKind.Relative)); ;
+        }
+
+        private static void TextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as SidebarTask).taskDescription.Content = (string)e.NewValue;
+        }
     }
+
+    public enum SiderbarTaskState
+	{
+        Default = 0,
+        Checkmark = 1,
+        Question = 2,
+	}
 }
