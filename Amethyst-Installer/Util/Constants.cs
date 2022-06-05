d2f4fcccd7ef0a35277ecb920e5cfbb17f44f6ba
Hyblocker <hyblocker@protonmail.com>
2022-06-06 01:27:33 +0200

    Create constants

---
+++ Amethyst-Installer/Util/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace amethyst_installer_gui
+{
+    /// <summary>
+    /// A static class containing constants such as directories
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Returns the logging directory for Amethyst
+        /// </summary>
+        public static string AmethystLogsDirectory
+        {
+            get
+            {
+                return Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Amethyst", "logs"));
+            }
+        }
+
+        /// <summary>
+        /// Returns a temporary directory which is 
+        /// </summary>
+        public static string AmethystTempDirectory
+        {
+            get
+            {
+                if (m_ameTmpDir == null || m_ameTmpDir == "")
+                    m_ameTmpDir = Path.GetTempPath();
+                return m_ameTmpDir;
+            }
+        }
+        private static string m_ameTmpDir = Path.GetTempPath();
+    }
+}
