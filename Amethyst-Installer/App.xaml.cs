fa1cee828b2744fe19f0b8c9e07d6117068973df
Hyblocker <hyblocker@protonmail.com>
2022-06-06 01:29:54 +0200

    Init logger

--- Amethyst-Installer/App.xaml.cs
+++ Amethyst-Installer/App.xaml.cs
@@ -1,8 +1,8 @@
-﻿using NAudio.CoreAudioApi;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows;
 
@@ -13,36 +13,20 @@ namespace amethyst_installer_gui
     /// </summary>
     public partial class App : Application
     {
-        const string KinectV1MicrophoneFriendlyName = "Kinect USB Audio";
-        const string KinectV2MicrophoneFriendlyName = "Xbox NUI Sensor";
-
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            // Initialize logger
+            string logFileDate = DateTime.Now.ToString("yyyymmdd-hhmmss.ffffff");
+            Logger.Init(Path.GetFullPath(Path.Combine(Constants.AmethystLogsDirectory, $"Amethyst_Installer_{logFileDate}.log")));
+
             // TODO: Replace with WinUI3-esque custom dialog box
             MessageBox.Show("big chungus", "no way");
 
             // Check if the Kinect microphone is muted, and if so, prompt the user to enable it.
-            var enumerator = new MMDeviceEnumerator();
-            foreach (MMDevice wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.All))
+            if (Installer.KinectUtil.KinectMicrophoneDisabled())
             {
-                // Skip devices which aren't plugged in (otherwise we'd get a COM Exception upon querying their friendly names)
-                if (wasapi.State == DeviceState.NotPresent)
-                    continue;
-                // Skip "Render" devices, i.e. Playback devices like headphone, speakers, etc.
-                if (wasapi.DataFlow == DataFlow.Render)
-                    continue;
-                
-                if (wasapi.DeviceFriendlyName == KinectV1MicrophoneFriendlyName || wasapi.DeviceFriendlyName == KinectV2MicrophoneFriendlyName)
-                {
-                    // Debug
-                    Console.WriteLine($"DataFlow: { wasapi.DataFlow } FriendlyName: { wasapi.FriendlyName } DeviceFriendlyName: { wasapi.DeviceFriendlyName } State: { wasapi.State } ID: { wasapi.ID }");
-
-                    if (wasapi.State != DeviceState.Active)
-                    {
-                        // Open sound control panel on the recording tab
-                        System.Diagnostics.Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL mmsys.cpl,,1");
-                    }
-                }
+                // Open sound control panel on the recording tab
+                System.Diagnostics.Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL mmsys.cpl,,1");
             }
         }
     }
