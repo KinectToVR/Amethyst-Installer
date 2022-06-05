2970b4d80892981f364cf3873fd9704612af0853
Hyblocker <hyblocker@protonmail.com>
2022-06-06 01:29:42 +0200

    Implement page switching

--- Amethyst-Installer/MainWindow.xaml.cs
+++ Amethyst-Installer/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using amethyst_installer_gui.Controls;
+using amethyst_installer_gui.Installer;
+using amethyst_installer_gui.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +22,23 @@ namespace amethyst_installer_gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<InstallerState, IInstallerPage> Pages = new Dictionary<InstallerState, IInstallerPage>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Init pages
+            Pages.Add(InstallerState.Welcome, new PageWelcome());
+            Pages.Add(InstallerState.InstallOptions, new PageInstallOptions());
+            Pages.Add(InstallerState.InstallDestination, new PageInstallDestination());
+            Pages.Add(InstallerState.SystemRequirements, new PageSystemRequirements());
+            Pages.Add(InstallerState.Downloading, new PageDownloading());
+            Pages.Add(InstallerState.Installation, new PageInstallation());
+            Pages.Add(InstallerState.Done, new PageDone());
+            
+            // Set default page to welcome
+            SetTab(InstallerState.Welcome);
         }
 
         #region Win UI 3 Window Functionality
@@ -36,12 +53,57 @@ namespace amethyst_installer_gui
         // Titlebar buttons
         private void Close_Click(object sender, RoutedEventArgs e)
         {
+            Logger.Info($"User closed installer!");
             Close();
         }
 
         private void Minimise_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        #endregion
+
+        #region Installer pages
+
+        public IInstallerPage CurrentInstallerPage
+        {
+            get { return (IInstallerPage)GetValue(CurrentInstallerPageProperty); }
+            set { SetValue(CurrentInstallerPageProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentInstallerPageProperty =
+            DependencyProperty.Register("CurrentInstallerPage", typeof(IInstallerPage), typeof(MainWindow), new PropertyMetadata(null, new PropertyChangedCallback(CurrentInstallerPageChanged)));
+
+
+        private static void CurrentInstallerPageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as MainWindow).PageView.Content = (IInstallerPage)e.NewValue;
+        }
+
+        public void SetTab(InstallerState destinatonTab)
+        {
+            CurrentInstallerPage = Pages[destinatonTab];
+            CurrentInstallerPage.OnSelected();
+            Logger.Info($"Changing installer state to {destinatonTab}");
+
+            windowTitle.Text = CurrentInstallerPage.GetTitle();
+
+            // Update checkmarks in sidebar
+            // IDK how we're going to handle ?s yet
+            sidebar_welcome.State           = destinatonTab < InstallerState.Welcome               ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_installOptions.State    = destinatonTab < InstallerState.InstallOptions        ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_location.State          = destinatonTab < InstallerState.InstallDestination    ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_sysreq.State            = destinatonTab < InstallerState.SystemRequirements    ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_download.State          = destinatonTab < InstallerState.Downloading           ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_install.State           = destinatonTab < InstallerState.Installation          ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+            sidebar_done.State              = destinatonTab < InstallerState.Done                  ? SiderbarTaskState.Default : SiderbarTaskState.Checkmark;
+        }
+
+        private void ActionButtonPrimary_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: route into currentpage?
+            SetTab(CurrentInstallerPage.GetInstallerState() + 1);
         }
 
         #endregion
