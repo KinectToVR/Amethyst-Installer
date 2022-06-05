103a44964898b416c49e4e01036583ebfdbb982c
Hyblocker <hyblocker@protonmail.com>
2022-06-05 11:46:23 +0200

    Dynamic version number

---
+++ Amethyst-Installer/Util/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amethyst_installer_gui
+{
+    public static class Util
+    {
+        /// <summary>
+        /// Returns the version number of Amethyst Installer
+        /// </summary>
+        public static string InstallerVersionString
+        {
+            get { 
+                string verison = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                return "Amethyst Installer v" + verison.Remove(verison.Length - 2);
+            }
+        }
+    }
+}
