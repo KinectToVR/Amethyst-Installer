f752a2ee1ebe8be63387c681748f3191121821a2
Hyblocker <hyblocker@protonmail.com>
2022-06-06 01:27:03 +0200

    Add Logger

---
+++ Amethyst-Installer/Util/Logger.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace amethyst_installer_gui
+{
+    /// <summary>
+    /// A logging class which mimics GLOG's trace style. While not highly configurable, this is
+    /// designed to be a simple drag and drop logger which relies solely on built-in C# APIs
+    /// </summary>
+    public static class Logger
+    {
+
+        // This is used instead of Thread.CurrentThread.ManagedThreadId since it returns the OS thread rather than the managed thread
+        // Consider using ManagedThreadId instead of this if you have to run this on non-Windows platforms
+        // https://stackoverflow.com/a/1679270
+        [DllImport("Kernel32", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
+        private static extern int GetCurrentWin32ThreadId();
+
+        private static string s_logFilePath;
+
+        #region Log Functions
+
+        public static void Info(string text, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(text, "I", lineNumber, filePath, memberName));
+        }
+
+        public static void Info(object obj, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(obj.ToString(), "I", lineNumber, filePath, memberName));
+        }
+
+        public static void Warn(string text, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(text, "W", lineNumber, filePath, memberName), ConsoleColor.Yellow);
+        }
+
+        public static void Warn(object obj, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(obj.ToString(), "W", lineNumber, filePath, memberName), ConsoleColor.Yellow);
+        }
+
+        public static void Error(string text, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(text, "E", lineNumber, filePath, memberName), ConsoleColor.Red);
+        }
+
+        public static void Error(object obj, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(obj.ToString(), "E", lineNumber, filePath, memberName), ConsoleColor.Red);
+        }
+
+        public static void Fatal(string text, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(text, "F", lineNumber, filePath, memberName), ConsoleColor.DarkRed);
+        }
+
+        public static void Fatal(object obj, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        {
+            LogInternal(FormatToLogMessage(obj.ToString(), "F", lineNumber, filePath, memberName), ConsoleColor.DarkRed);
+        }
+
+        #endregion
+
+        #region Logger Internals
+
+        /// <summary>
+        /// Initializes the logger
+        /// </summary>
+        public static void Init(string filePath = "")
+        {
+            if (filePath == "")
+                s_logFilePath = $"{Assembly.GetCallingAssembly().GetName()}_{DateTime.Now.ToString("yyyymmdd-hhmmss.ffffff")}.log";
+            else
+                s_logFilePath = filePath;
+
+            s_logFilePath = Path.GetFullPath(filePath);
+            string dir = Path.GetFullPath(Path.GetDirectoryName(s_logFilePath));
+
+            bool loggingPathDidntExist = false;
+            if (!Directory.Exists(dir)) {
+                Directory.CreateDirectory(dir);
+                loggingPathDidntExist = true;
+            }
+
+            // Create the file, rest of the methods will append
+            File.Create(s_logFilePath).Close();
+
+            if (loggingPathDidntExist)
+                LogInternal($"Created logging directory at \"{dir}\"");
+            LogInternal($"Log file created at: {DateTime.Now.ToString("yyyy/mm/dd hh:mm:ss")}");
+            LogInternal($"Running on machine: {Environment.MachineName}");
+            LogInternal("Running duration (h:mm:ss): 0:00:00");
+            LogInternal("Log line format: [IWEF]yyyymmdd hh:mm:ss.ffffff threadid file::member:line] msg");
+        }
+
+        private static string FormatToLogMessage(string message, string level, int lineNumber, string filePath, string memberName)
+        {
+            return $"{level}{DateTime.Now.ToString("yyyymmdd hh:mm:ss.ffffff")} {GetCurrentWin32ThreadId()} {Path.GetFileName(filePath)}::{memberName}:{lineNumber}] {message}";
+        }
+
+        private static void LogInternal(string message)
+        {
+            if (s_logFilePath == null)
+                throw new InvalidOperationException("Tried logging something without calling Logger.Init()! Aborting...");
+            Console.ResetColor();
+            Console.WriteLine(message);
+            File.AppendAllLines(s_logFilePath, new[] { message });
+        }
+
+        private static void LogInternal(string message, ConsoleColor color)
+        {
+            if (s_logFilePath == null)
+                throw new InvalidOperationException("Tried logging something without calling Logger.Init()! Aborting...");
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            File.AppendAllLines(s_logFilePath, new[] { message });
+        }
+        
+        #endregion
+    }
+}
