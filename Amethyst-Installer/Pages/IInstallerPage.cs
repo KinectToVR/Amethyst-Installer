91d769ed47250ac0cd47b941e7d875370f8c6523
Hyblocker <hyblocker@protonmail.com>
2022-06-07 15:01:18 +0200

    Add more methods to pages

--- Amethyst-Installer/Pages/IInstallerPage.cs
+++ Amethyst-Installer/Pages/IInstallerPage.cs
@@ -1,4 +1,5 @@
 ﻿using amethyst_installer_gui.Installer;
+using System.Windows;
 
 namespace amethyst_installer_gui.Pages
 {
@@ -6,6 +7,19 @@ namespace amethyst_installer_gui.Pages
     {
         InstallerState GetInstallerState();
         string GetTitle();
+
+        /// <summary>
+        /// Called when a page takes focus
+        /// </summary>
+        void OnFocus();
+        /// <summary>
+        /// Called when a page gets loaded. Only gets called once
+        /// </summary>
         void OnSelected();
+
+        // Button handling
+        void OnButtonPrimary(object sender, RoutedEventArgs e);
+        void OnButtonSecondary(object sender, RoutedEventArgs e);
+        void OnButtonTertiary(object sender, RoutedEventArgs e);
     }
 }
