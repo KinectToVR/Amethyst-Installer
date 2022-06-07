6b739a9dfe8d63519aa832376f0b6a72caad1a2c
Hyblocker <hyblocker@protonmail.com>
2022-06-07 15:04:34 +0200

    Update App.xaml.cs

--- Amethyst-Installer/App.xaml.cs
+++ Amethyst-Installer/App.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+﻿using amethyst_installer_gui.Installer;
+using amethyst_installer_gui.Pages;
+using System;
 using System.IO;
-using System.Linq;
 using System.Windows;
+
+using AppWindow = amethyst_installer_gui.MainWindow;
 
 namespace amethyst_installer_gui
 {
@@ -16,14 +16,15 @@ namespace amethyst_installer_gui
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             // Initialize logger
-            string logFileDate = DateTime.Now.ToString("yyyymmdd-hhmmss.ffffff");
+            string logFileDate = DateTime.Now.ToString("yyyyMMdd-hhmmss.ffffff");
             Logger.Init(Path.GetFullPath(Path.Combine(Constants.AmethystLogsDirectory, $"Amethyst_Installer_{logFileDate}.log")));
 
             // TODO: Replace with WinUI3-esque custom dialog box
             MessageBox.Show("big chungus", "no way");
 
+            // TODO: Move to sysreq
             // Check if the Kinect microphone is muted, and if so, prompt the user to enable it.
-            if (Installer.KinectUtil.KinectMicrophoneDisabled())
+            if (KinectUtil.KinectMicrophoneDisabled())
             {
                 // Open sound control panel on the recording tab
                 System.Diagnostics.Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL mmsys.cpl,,1");
