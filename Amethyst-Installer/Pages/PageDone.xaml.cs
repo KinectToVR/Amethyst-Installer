244aa97919a4bf24e36221fa8a1e33bdcef937fb
Hyblocker <hyblocker@protonmail.com>
2022-06-07 15:02:43 +0200

    Update PageDone.xaml.cs

--- Amethyst-Installer/Pages/PageDone.xaml.cs
+++ Amethyst-Installer/Pages/PageDone.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using amethyst_installer_gui.Installer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace amethyst_installer_gui.Pages
 {
@@ -36,9 +25,23 @@ namespace amethyst_installer_gui.Pages
             return Properties.Resources.Page_Done_Title;
         }
 
-        public void OnSelected()
+        public void OnButtonPrimary(object sender, RoutedEventArgs e)
         {
-            
+            throw new InvalidOperationException("AMONG US IS AN AWEOSME VIDEOGAME I FUCKING LOVE AMONGUS");
         }
+
+        public void OnSelected() {}
+
+        // Force only the first button to have focus
+        public void OnFocus()
+        {
+            MainWindow.Instance.ActionButtonPrimary.Visibility = Visibility.Visible;
+            MainWindow.Instance.ActionButtonPrimary.Content = Properties.Resources.Installer_Action_Next;
+            MainWindow.Instance.ActionButtonSecondary.Visibility = Visibility.Hidden;
+            MainWindow.Instance.ActionButtonTertiary.Visibility = Visibility.Hidden;
+        }
+
+        public void OnButtonSecondary(object sender, RoutedEventArgs e) {}
+        public void OnButtonTertiary(object sender, RoutedEventArgs e) {}
     }
 }
