e0469faf347af15fdcd4cbbd1dfe55adb0a3294c
Hyblocker <hyblocker@protonmail.com>
2022-06-10 22:28:28 +0200

    Add downloader utility

---
+++ Amethyst-Installer/Util/Download.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amethyst_installer_gui.Util {
+    internal class Download {
+        public void DownloadFile(string url, string path, float timeout = 10) {
+            Process.Start(new ProcessStartInfo() {
+                FileName = "curl.exe",
+                Arguments = $"--connect-timeout {timeout.ToString("n2")}",
+            });
+        }
+    }
+}
