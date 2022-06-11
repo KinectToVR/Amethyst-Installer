e1a9ffb2df0c074394a86cbb9fb72f358e623713
Hyblocker <hyblocker@protonmail.com>
2022-06-11 19:52:44 +0200

    Add cURL download wrapper

--- Amethyst-Installer/Util/Download.cs
+++ Amethyst-Installer/Util/Download.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace amethyst_installer_gui.Util {
+namespace amethyst_installer_gui {
     internal class Download {
-        public void DownloadFile(string url, string path, float timeout = 10) {
+        public void DownloadFile(string url, string path = null, float timeout = 10) {
+            var AAA = $"--connect-timeout {timeout.ToString("n1")} -L -O";
             Process.Start(new ProcessStartInfo() {
                 FileName = "curl.exe",
-                Arguments = $"--connect-timeout {timeout.ToString("n2")}",
+                Arguments = $"--connect-timeout {timeout.ToString("n2")} --location --remote-name {url}",
+                WorkingDirectory = path ?? Directory.GetCurrentDirectory()
             });
         }
     }
