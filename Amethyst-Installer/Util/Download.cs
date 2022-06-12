b6acf7236d1c6b68a13ff5346cddca78819dfeae
Hyblocker <hyblocker@protonmail.com>
2022-06-13 01:52:03 +0200

    Improve cURL commands

--- Amethyst-Installer/Util/Download.cs
+++ Amethyst-Installer/Util/Download.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace amethyst_installer_gui {
     internal class Download {
-        public void DownloadFile(string url, string path = null, float timeout = 10) {
-            var AAA = $"--connect-timeout {timeout.ToString("n1")} -L -O";
+        public void DownloadFile(string url, string filename = null, string path = null, float timeout = 10) {
+            var AAA = $"--connect-timeout {timeout.ToString("n2")} -LO {(filename == null ? filename.Trim() + " " : "")}{url.Trim()}";
             Process.Start(new ProcessStartInfo() {
                 FileName = "curl.exe",
-                Arguments = $"--connect-timeout {timeout.ToString("n2")} --location --remote-name {url}",
+                Arguments = $"--connect-timeout {timeout.ToString("n2")} -LO {(filename == null ? filename.Trim() + " " : "")}{url.Trim()}",
                 WorkingDirectory = path ?? Directory.GetCurrentDirectory()
             });
         }
