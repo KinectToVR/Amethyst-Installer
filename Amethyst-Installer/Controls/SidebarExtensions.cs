bfb0a11615834d1ff39749bc8a7bdd24b149bd68
Hyblocker <hyblocker@protonmail.com>
2022-06-13 23:28:34 +0200

    Fix scrollbar icons not aligning properly

---
+++ Amethyst-Installer/Controls/SidebarExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace amethyst_installer_gui.Controls {
+    public class SidebarExtensions {
+        public static readonly DependencyProperty ScrollIconPaddingProperty =
+        DependencyProperty.RegisterAttached("ScrollIconPadding", typeof(Thickness), typeof(SidebarExtensions), new PropertyMetadata(default(Thickness)));
+
+        public static void SetScrollIconPadding(UIElement element, Thickness value) {
+            element.SetValue(ScrollIconPaddingProperty, value);
+        }
+
+        public static Thickness GetScrollIconPadding(UIElement element) {
+            return ( Thickness ) element.GetValue(ScrollIconPaddingProperty);
+        }
+    }
+}
