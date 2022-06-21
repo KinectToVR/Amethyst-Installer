f4fc9212e3a1e90e5d896fd8b074757e122c0730
Hyblocker <hyblocker@protonmail.com>
2022-06-21 03:23:11 +0200

    Add vector class

---
+++ Amethyst-Installer/Util/Vectors.cs
@@ -0,0 +1,13 @@
+﻿namespace amethyst_installer_gui {
+    public struct Vec2 {
+        public double x;
+        public double y;
+
+        public Vec2(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vec2(double mag) : this(mag, mag) { }
+    }
+}
