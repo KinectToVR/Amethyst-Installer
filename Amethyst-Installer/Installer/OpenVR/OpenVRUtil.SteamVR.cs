ab6cf5c69fd29702da8b87fa6176c6d41d76b872
Hyblocker <hyblocker@protonmail.com>
2022-06-21 03:24:19 +0200

    Playspace bounds detection

--- Amethyst-Installer/Installer/OpenVR/OpenVRUtil.SteamVR.cs
+++ Amethyst-Installer/Installer/OpenVR/OpenVRUtil.SteamVR.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -95,6 +96,97 @@ namespace amethyst_installer_gui.Installer {
                 string newSteamVrSettingsContents = JsonConvert.SerializeObject(s_steamvrSettings, Formatting.Indented);
                 File.WriteAllText(s_steamvrSettingsPath, newSteamVrSettingsContents);
             }
+        }
+
+        public static Vec2 GetPlayspaceBounds() {
+
+            // In a perfect world, we would just run
+            // 
+            // float sizeX = 0f, sizeY = 0f;
+            // Valve.VR.EVRInitError initErr = Valve.VR.EVRInitError.None;
+            // Valve.VR.OpenVR.Init(ref initErr);
+            // Valve.VR.OpenVR.Chaperone.GetPlayAreaSize(ref sizeX, ref sizeY);
+
+            // However, we are using SteamVR, so instead WE DO THIS:
+            double sizeX = , sizeY = 0.0;
+
+            // TODO: not be a lazy piece of shit
+            // Oculus => config/oculus/driver_oculus.vrchap
+            // Lighthouse => config/chaperone_info.vrchap
+            // WMR => config/chaperone_info.vrchap
+
+            string vrchapPath = "";
+            bool isSteamVrDefaultChaperoneFile = false;
+            if ( s_openvrpaths != null ) {
+                // Try getting SteamVR Settings from OpenVR Paths
+                for ( int i = 0; i < s_openvrpaths.config.Count; i++ ) {
+                    if ( Directory.Exists(s_openvrpaths.config[i]) ) {
+                        vrchapPath = s_openvrpaths.config[i];
+                    }
+                }
+            } else {
+                // Fallback to Steam Directory
+                var steamInstallDirectory = GetSteamInstallDirectory();
+                if ( Directory.Exists(steamInstallDirectory) ) {
+                    vrchapPath = Path.Combine(steamInstallDirectory, "config");
+                }
+            }
+
+            switch ( HmdType ) {
+                case VRHmdType.Rift:
+                case VRHmdType.RiftS:
+                case VRHmdType.Quest:
+                case VRHmdType.Quest2:
+                    // Oculus
+                    vrchapPath = Path.Combine(vrchapPath, "oculus", "driver_oculus.vrchap");
+                    break;
+                case VRHmdType.Vive:
+                case VRHmdType.VivePro:
+                case VRHmdType.Pimax:
+                case VRHmdType.Index:
+                case VRHmdType.Deckard:
+                    // Lighthouse
+
+                case VRHmdType.WMR:
+                    // WMR
+
+                default:
+                    // Assume the playspace data is stored in chaperone_info.vrchap
+                    vrchapPath = Path.Combine(vrchapPath, "chaperone_info.vrchap");
+                    isSteamVrDefaultChaperoneFile = true;
+                    break;
+            }
+
+            if ( !File.Exists(vrchapPath) && isSteamVrDefaultChaperoneFile == false ) {
+                vrchapPath = Path.Combine(vrchapPath, "chaperone_info.vrchap");
+            }
+            OpenVrPlayspace steamVrChaperone = JsonConvert.DeserializeObject<OpenVrPlayspace>(File.ReadAllText(vrchapPath));
+            if ( steamVrChaperone != null && steamVrChaperone.Universes != null ) {
+                int latestChaperoneIndex = 0;
+                // get the newest steamvr chaperone universe
+                // this assumes that the higher the numerical value of the "universeID" string the newer the playspace is
+                // we don't check via date because that would require a custom DateTime parse and this installer is already
+                // over-engineered enough
+                ulong latestID = 0, currentID = 0; // declared here because of garbage collection hell
+                bool latestSuccess = false, currentSuccess = false;
+                for ( int i = 0; i < steamVrChaperone.Universes.Count; i++ ) {
+                    if ( steamVrChaperone.Universes[i].UniverseID != null && steamVrChaperone.Universes[i].PlayArea != null ) {
+                        // reset
+                        latestSuccess = false;
+                        currentSuccess = false;
+
+                        latestSuccess = ulong.TryParse(steamVrChaperone.Universes[latestChaperoneIndex].UniverseID, out latestID);
+                        currentSuccess = ulong.TryParse(steamVrChaperone.Universes[i].UniverseID, out currentID);
+                        if ( latestSuccess && currentSuccess && currentID > latestID ) {
+                            latestChaperoneIndex = i;
+                        }
+                    }
+                }
+                sizeX = ( double ) steamVrChaperone.Universes[latestChaperoneIndex].PlayArea[0];
+                sizeY = ( double ) steamVrChaperone.Universes[latestChaperoneIndex].PlayArea[1];
+            }
+
+            return new Vec2(sizeX, sizeY);
         }
     }
 }
