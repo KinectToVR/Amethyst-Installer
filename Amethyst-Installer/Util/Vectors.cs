7cf88656404373be8bafdf040bde2949aa9765b6
Hyblocker <hyblocker@protonmail.com>
2022-08-11 14:26:09 +0200

    Make Vector2 easier to use

--- Amethyst-Installer/Util/Vectors.cs
+++ Amethyst-Installer/Util/Vectors.cs
@@ -9,5 +9,16 @@
         }
 
         public Vec2(double mag) : this(mag, mag) { }
+
+        public override string ToString() {
+            return $"{{ {x}, {y} }}";
+        }
+
+        public static readonly Vec2 Zero    = new Vec2(0, 0);
+        public static readonly Vec2 One     = new Vec2(1, 1);
+        public static readonly Vec2 Left    = new Vec2(-1, 0);
+        public static readonly Vec2 Right   = new Vec2(1, 0);
+        public static readonly Vec2 Up      = new Vec2(0, 1);
+        public static readonly Vec2 Down    = new Vec2(0, -1);
     }
 }
