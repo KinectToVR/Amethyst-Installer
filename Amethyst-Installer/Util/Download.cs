1b21d2fc6c1efca493184772a7a90bcd75f4b812
Hyblocker <hyblocker@protonmail.com>
2022-08-17 03:14:21 +0200

    Cleanup

--- Amethyst-Installer/Util/Download.cs
+++ Amethyst-Installer/Util/Download.cs
@@ -27,27 +27,13 @@ namespace amethyst_installer_gui {
         /// <returns></returns>
         public static async Task DownloadFileAsync(string url, string filename = null, string path = null, Action<long> progress = null, float timeout = 10) {
 
-            // using ( var httpClient = new HttpClient() ) {
-                s_httpClient.Timeout = TimeSpan.FromSeconds(timeout);
+            s_httpClient.Timeout = TimeSpan.FromSeconds(timeout);
 
-                Logger.Info($"Downloading file {filename} from {url}...");
+            Logger.Info($"Downloading file {filename} from {url}...");
 
-                // Stream response = await httpClient.GetStreamAsync(url);
-
-                using ( var fileStream = File.OpenWrite(Path.GetFullPath(Path.Combine(path, filename))) ) {
-                    // await response.CopyToAsync(fileStream)
-                    await s_httpClient.DownloadAsync(url, fileStream, progress, (long)(timeout * 1000L));
-                }
-            // }
-
-            return;
-
-            var AAA = $"--connect-timeout {timeout.ToString("n2")} -LO {(filename == null ? filename.Trim() + " " : "")}{url.Trim()}";
-            Process.Start(new ProcessStartInfo() {
-                FileName = "curl.exe",
-                Arguments = $"--connect-timeout {timeout.ToString("n2")} -LO {( filename == null ? filename.Trim() + " " : "" )}{url.Trim()}",
-                WorkingDirectory = path ?? Directory.GetCurrentDirectory()
-            });
+            using ( var fileStream = File.OpenWrite(Path.GetFullPath(Path.Combine(path, filename))) ) {
+                await s_httpClient.DownloadAsync(url, fileStream, progress, ( long ) ( timeout * 1000L ));
+            }
         }
     }
 }
