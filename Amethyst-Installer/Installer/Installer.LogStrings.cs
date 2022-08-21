2e9725e93206432b5c142e56d6e9bc124f04c8e9
Hyblocker <hyblocker@protonmail.com>
2022-08-21 23:15:59 +0200

    Add install module log strings

---
+++ Amethyst-Installer/Installer/Installer.LogStrings.cs
@@ -0,0 +1,13 @@
+﻿namespace amethyst_installer_gui.Installer
+{
+    public static class LogStrings
+    {
+        public const string ExtractingAmethyst              = "Extracting Amethyst to {0}";
+        public const string ExtractAmethystFailed           = "Failed to extract Amethyst";
+        public const string RegisteringAmethystDriver       = "Registering Amethyst driver...";
+        public const string CheckingAmethystDriverConflicts = "Checking for conflicting SteamVR drivers...";
+        public const string InstalledAmethystSuccess        = "Successfully installed Amethyst!";
+
+        public const string ViewLogs                        = "Please view the logs for more info.";
+    }
+}
