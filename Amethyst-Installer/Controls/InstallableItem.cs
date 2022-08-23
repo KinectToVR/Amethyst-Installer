523f9be02232d9fdcd65d6d28cbbb6927a57227f
Hyblocker <hyblocker@protonmail.com>
2022-08-23 15:11:08 +0200

    Fix InstallableItem behaviour

--- Amethyst-Installer/Controls/InstallableItem.cs
+++ Amethyst-Installer/Controls/InstallableItem.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using amethyst_installer_gui.Properties;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -15,10 +17,11 @@ namespace amethyst_installer_gui.Controls {
 
         private TextBlock   itemTitle;
         private TextBlock   itemDescription;
-        private CheckBox    itemCheckbox;
+        public CheckBox     itemCheckbox;
         private Border      controlContainer;
 
         public MouseButtonEventHandler OnMouseClickReleased;
+        public RoutedEventHandler OnToggled;
 
         public bool Checked {
             get { return ( bool ) GetValue(CheckedProperty); }
@@ -27,7 +30,18 @@ namespace amethyst_installer_gui.Controls {
 
         public static readonly DependencyProperty CheckedProperty =
             DependencyProperty.Register("Checked", typeof(bool), typeof(InstallableItem), new UIPropertyMetadata(false, new PropertyChangedCallback(CheckedChanged)));
-        
+
+        private static void CheckedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            var thisControl = d as InstallableItem;
+            if ( thisControl.itemCheckbox == null )
+                return;
+            if ( !thisControl.Disabled ) {
+                thisControl.itemCheckbox.IsChecked = ( bool ) e.NewValue;
+
+                if ( thisControl.OnToggled != null )
+                    thisControl.OnToggled(thisControl, new RoutedEventArgs());
+            }
+        }
 
         public bool Disabled {
             get { return ( bool ) GetValue(DisabledProperty); }
@@ -53,12 +67,6 @@ namespace amethyst_installer_gui.Controls {
         public static readonly DependencyProperty DescriptionProperty =
             DependencyProperty.Register("Description", typeof(string), typeof(InstallableItem), new UIPropertyMetadata("Lorem ipsum dolor sit amet", new PropertyChangedCallback(DescriptionChanged)));
 
-
-        private static void CheckedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-            if ( ( d as InstallableItem ).itemCheckbox == null )
-                return;
-            ( d as InstallableItem ).itemCheckbox.IsChecked = ( bool ) e.NewValue;
-        }
         
         private static void DisabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
             if ( ( d as InstallableItem ).itemCheckbox == null )
@@ -87,12 +95,14 @@ namespace amethyst_installer_gui.Controls {
             controlContainer = GetTemplateChild("controlContainer") as Border;
 
             // Apply props
-            itemCheckbox.IsChecked = Checked;
+            // itemCheckbox.IsChecked = Checked;
             itemCheckbox.IsEnabled = !Disabled;
             itemTitle.Text = Title;
             itemDescription.Text = Description;
 
             controlContainer.MouseLeftButtonUp += container_MouseLeftButtonDown;
+            itemCheckbox.Checked += itemCheckbox_CheckToggled;
+            itemCheckbox.Unchecked += itemCheckbox_CheckToggled;
         }
 
         private void container_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
@@ -100,9 +110,16 @@ namespace amethyst_installer_gui.Controls {
             // Background = #FF474444
             Background = new SolidColorBrush(Color.FromRgb(0x47, 0x44, 0x44));
 
-            Checked = !Checked;
+            if ( !Disabled ) {
+                Checked = !Checked;
+            }
             if ( OnMouseClickReleased != null)
                 OnMouseClickReleased(this, e);
+        }
+        private void itemCheckbox_CheckToggled(object sender, RoutedEventArgs e) {
+
+            if ( OnToggled != null)
+                OnToggled(this, e);
         }
     }
 }
