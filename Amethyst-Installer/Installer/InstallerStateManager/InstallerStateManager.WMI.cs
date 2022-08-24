4b23a04ca17f6b06af3e02acf6b23c41b6a61226
Hyblocker <hyblocker@protonmail.com>
2022-08-24 19:00:13 +0200

    WMI stuff init

---
+++ Amethyst-Installer/Installer/InstallerStateManager/InstallerStateManager.WMI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amethyst_installer_gui.Installer {
+	/// <summary>
+	/// Class that handles the installer's global state.
+	/// UI updates this class' state, then this class calls all the actual logic.
+	/// </summary>
+	public static partial class InstallerStateManager {
+
+
+
+    }
+}
