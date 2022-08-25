8c252f58841461d78afc647ae050740217c5f404
Hyblocker <hyblocker@protonmail.com>
2022-08-25 11:34:19 +0200

    Cleanup

--- Amethyst-Installer/Util/Commands/ICommand.cs
+++ Amethyst-Installer/Util/Commands/ICommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace amethyst_installer_gui.Commands {
+﻿namespace amethyst_installer_gui.Commands {
     /// <summary>
     /// A generic command the installer supports
     /// </summary>
