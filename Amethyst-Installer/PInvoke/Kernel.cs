b92caaf47b867ec32b447a38ea3916d1923564a2
Hyblocker <hyblocker@protonmail.com>
2022-08-26 13:33:33 +0200

    Unicode in my terminal!

--- Amethyst-Installer/PInvoke/Kernel.cs
+++ Amethyst-Installer/PInvoke/Kernel.cs
@@ -10,5 +10,37 @@ namespace amethyst_installer_gui.PInvoke {
 
         [DllImport("Kernel32.dll")]
         public static extern bool AttachConsole(int processId);
+
+
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+        private const uint DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetLastError();
+
+
+        public static void EnableAnsiCmd() {
+            var iStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+            if ( !GetConsoleMode(iStdOut, out uint outConsoleMode) ) {
+                Console.Error.WriteLine("Failed to get output console mode");
+                return;
+            }
+
+            outConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+            if ( !SetConsoleMode(iStdOut, outConsoleMode) ) {
+                Console.Error.WriteLine($"Failed to set output console mode, error code: {GetLastError()}");
+                return;
+            }
+        }
     }
 }
