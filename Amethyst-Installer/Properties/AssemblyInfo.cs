df61133ffb323674c2ad14151cd2676a7a5c6d97
Hyblocker <hyblocker@protonmail.com>
2022-08-26 19:32:55 +0200

    Update AssemblyInfo.cs

--- Amethyst-Installer/Properties/AssemblyInfo.cs
+++ Amethyst-Installer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Amethyst Installer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Amethyst Installer Preview")]
+[assembly: AssemblyDescription("The installer for Amethyst")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("amethyst_installer_gui")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("K2VR Team")]
+[assembly: AssemblyProduct("Amethyst Installer Preview")]
+[assembly: AssemblyCopyright("Copyright © K2VR Team 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
