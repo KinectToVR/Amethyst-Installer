4dff5a5c35e056b18ae1e6972ebe80a6e6978cf0
Hyblocker <hyblocker@protonmail.com>
2022-08-29 01:20:39 +0200

    [STUB] Add update command

---
+++ Amethyst-Installer/Util/Commands/CommandUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace amethyst_installer_gui.Commands {
+    public class CommandUpdate : ICommand {
+
+        public string Command { get => "update"; set { } }
+        public string Description { get => "Attempts to update Amethyst"; set { } }
+        public string[] Aliases { get => new string[] { "u" }; set { } }
+
+        public bool Execute(string parameters) {
+
+            return true;
+        }
+    }
+}
