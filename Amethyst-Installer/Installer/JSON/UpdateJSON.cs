4cc5c5158ba078e7d36798b77eb0f7ab018e9398
Hyblocker <hyblocker@protonmail.com>
2022-08-31 16:56:17 +0200

    Detect modules to update

---
+++ Amethyst-Installer/Installer/JSON/UpdateJSON.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace amethyst_installer_gui.Installer {
+    public class UpdateJSON {
+        [JsonProperty("version")]
+        public int Version { get; set; }
+        [JsonProperty("version_string")]
+        public string VersionString { get; set; }
+        [JsonProperty("changelog")]
+        public string[] Changelog { get; set; }
+    }
+}
--- Amethyst-Installer/Util/Commands/CommandUpdate.cs
+++ Amethyst-Installer/Util/Commands/CommandUpdate.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using amethyst_installer_gui.Installer;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -15,9 +17,99 @@ namespace amethyst_installer_gui.Commands {
         public string Description { get => "Attempts to update Amethyst"; set { } }
         public string[] Aliases { get => new string[] { "u" }; set { } }
 
+        private static Dictionary<string, UpdateJSON> UpdateEndpoint;
+
         public bool Execute(string parameters) {
 
-            return true;
+            App.Init();
+
+            // Try fetching the updates endpoint
+            try {
+                FetchUpdates();
+            } catch (Exception e) {
+                Logger.Fatal("An exception was thrown while trying to fetch the update list!");
+                Logger.Fatal(Util.FormatException(e));
+                Util.ShowMessageBox(Localisation.Updating_FailedReachApi, Localisation.Updating_InitFailed);
+                return true;
+            }
+
+            // Load the installer config
+            // @TODO: Amogus
+            string installerConfigPath = Path.GetFullPath(Path.Combine(Constants.AmethystConfigDirectory, "Modules.json"));
+            Dictionary<string, int> config;
+            if ( !File.Exists(installerConfigPath) ) {
+                // Assume only amethyst
+                config = new Dictionary<string, int>();
+                config.Add("amethyst", 0);
+            } else {
+
+                // The file exists! Try reading it
+                try {
+
+                    string fileContents = File.ReadAllText(installerConfigPath);
+                    config = JsonConvert.DeserializeObject<Dictionary<string, int>>(fileContents);
+                        
+                } catch (Exception e) {
+                    Logger.Fatal($"Failed to load or parse file \"{installerConfigPath}\"!");
+                    Logger.Fatal(Util.FormatException(e));
+
+                    // Assume file is corrupt
+                    config = new Dictionary<string, int>();
+                    config.Add("amethyst", 0);
+                }
+            }
+
+            // We have a valid config!
+            List<string> modulesToUpdate = new List<string>();
+
+            // Go through each key, mark it as "requires update"
+            foreach ( var x in UpdateEndpoint ) {
+                Logger.Info($"{x.Key} :: {x.Value.Version} ; {x.Value.VersionString}");
+                if ( config.ContainsKey(x.Key) ) {
+                    if ( config[x.Key] < x.Value.Version ) {
+                        modulesToUpdate.Add(x.Key);
+                    }
+                }
+            }
+
+            // Now queue the modules to the updater
+            // @TODO: Queue modules to updater
+
+            App.InitialPage = InstallerState.Updating;
+
+            return false;
+        }
+
+
+        private static void FetchUpdates() {
+
+#if !DIST
+            string txtResponse = string.Empty;
+            using ( var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("amethyst_installer_gui.ame-installer-sample-api-response-update.json") ) {
+                using ( StreamReader reader = new StreamReader(resource) ) {
+                    txtResponse = reader.ReadToEnd();
+                }
+            }
+
+            UpdateEndpoint = JsonConvert.DeserializeObject<Dictionary<string, UpdateJSON>>(txtResponse);
+#else
+            try {
+                Logger.Info("Checking latest versions...");
+                var packagesJson = Download.GetStringAsync(Constants.ApiDomain + "update");
+
+                UpdateEndpoint = JsonConvert.DeserializeObject<Dictionary<string, UpdateJSON>>(packagesJson);
+                Logger.Info("Fetched latest versions successfully!");
+                Logger.Info("Checking for updates...");
+                Logger.Info("Determined update list!");
+
+            } catch ( Exception e ) {
+                Logger.Fatal("Failed to fetch update list!");
+                Logger.Fatal(Util.FormatException(e));
+
+                // @TODO: Explode?
+                throw new Exception();
+            }
+#endif
         }
     }
 }
