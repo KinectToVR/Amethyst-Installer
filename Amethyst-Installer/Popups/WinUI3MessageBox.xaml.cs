cbf58e62e9ae69e8dbe8dfd0645a9442597986ae
Hyblocker <hyblocker@protonmail.com>
2022-09-20 11:57:48 +0200

    Give dialog boxes Mica!

--- Amethyst-Installer/Popups/WinUI3MessageBox.xaml.cs
+++ Amethyst-Installer/Popups/WinUI3MessageBox.xaml.cs
@@ -1,6 +1,8 @@
 using amethyst_installer_gui.PInvoke;
+using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace amethyst_installer_gui.Popups {
     /// <summary>
@@ -12,26 +14,44 @@ namespace amethyst_installer_gui.Popups {
 
         public WinUI3MessageBox(string title, string caption, string buttonPrimaryText, string buttonSecondaryText, string buttonTertiaryText) {
             InitializeComponent();
+            ContentRendered += Window_ContentRendered;
 
             // Apply props
             titleContainer.Text = title;
-            Title = title;
+            Title               = title;
 
             message.Inlines.Clear();
             message.Text = caption;
 
-            dialogButton_Primary.Content = buttonPrimaryText;
-            dialogButton_Secondary.Content = buttonSecondaryText;
-            dialogButton_Tertiary.Content = buttonTertiaryText;
-
-            dialogButton_Primary.Visibility = buttonPrimaryText.Length == 0 ? Visibility.Hidden : Visibility.Visible;
-            dialogButton_Secondary.Visibility = buttonSecondaryText.Length == 0 ? Visibility.Hidden : Visibility.Visible;
-            dialogButton_Tertiary.Visibility = buttonTertiaryText.Length == 0 ? Visibility.Hidden : Visibility.Visible;
+            dialogButton_Primary.Content    = buttonPrimaryText;
+            dialogButton_Secondary.Content  = buttonSecondaryText;
+            dialogButton_Tertiary.Content   = buttonTertiaryText;
 
             // Handle visibility
+            dialogButton_Primary.Visibility     = buttonPrimaryText.Length      == 0 ? Visibility.Hidden : Visibility.Visible;
+            dialogButton_Secondary.Visibility   = buttonSecondaryText.Length    == 0 ? Visibility.Hidden : Visibility.Visible;
+            dialogButton_Tertiary.Visibility    = buttonTertiaryText.Length     == 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        // Setup theme related stuff using DWM
+        private void Window_ContentRendered(object sender, EventArgs e) {
 
             // Fix corners on Win11
             DWM.SetWindowCorners(this, CornerPreference.Round);
+
+            // @TODO: Theming!!
+            // Dark / Light mode
+            DWM.SetDarkMode(this, true);
+
+            // Mica some bitches
+            if ( DWM.EnableBackdropBlur(this) ) {
+
+                DWM.ExtendWindowChrome(this);
+            } else {
+                var color = ( ( SolidColorBrush ) Background ).Color;
+                color.A = 255;
+                Background = new SolidColorBrush(color);
+            }
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e) {
