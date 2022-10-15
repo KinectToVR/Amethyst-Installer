8723c59f346fa546f3380c8b8d60129c9aad03f0
Hyblocker <hyblocker@protonmail.com>
2022-10-15 09:21:21 +0200

    Cleanup

--- Amethyst-Installer/Installer/KinectUtil/KinectUtil.NotReady.cs
+++ Amethyst-Installer/Installer/KinectUtil/KinectUtil.NotReady.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using amethyst_installer_gui.PInvoke;
 using Microsoft.Kinect;
 
