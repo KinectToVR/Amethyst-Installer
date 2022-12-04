4c22f2b1cf5ec1931c6530deae27014239b50bb8
Hyblocker <hyblocker@protonmail.com>
2022-12-05 00:58:34 +0100

    Mom can we have dxc.exe at home?

--- Amethyst-Installer/DirectX/Shaders/_CompileShaders.bat
+++ Amethyst-Installer/DirectX/Shaders/_CompileShaders.bat
@@ -1,4 +1,7 @@
 @echo off
 call "%~dp0..\..\..\fxc.bat" /Tvs_4_0_level_9_1 /Evert /Fosimple_vert.cso simple.hlsl
 call "%~dp0..\..\..\fxc.bat" /Tps_4_0_level_9_1 /Efrag /Fosimple_frag.cso simple.hlsl
+
+REM Move to destination
+if not exist "%~dp0..\..\Resources\Shaders" mkdir "%~dp0..\..\Resources\Shaders"
 move /y "%~dp0\*.cso" "%~dp0..\..\Resources\Shaders"
--- fxc.bat
+++ fxc.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 
 call :GetWin10SdkDirHelper HKLM\SOFTWARE\Wow6432Node > nul 2>&1
 if errorlevel 1 call :GetWin10SdkDirHelper HKCU\SOFTWARE\Wow6432Node > nul 2>&1
@@ -10,6 +10,7 @@ setlocal
 set PATH="%WindowsSdkDir%\bin\10.0.22621.0\x86";%PATH%
 fxc.exe %*
 endlocal
+exit /b 0
 
 :GetWin10SdkDirHelper
 
