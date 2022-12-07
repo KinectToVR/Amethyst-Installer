fa0f6fec23a83c53ae12bfe5e941753dce4c2290
Hyblocker <hyblocker@protonmail.com>
2022-12-07 11:59:59 +0100

    Compile on shader model 5.0

--- Amethyst-Installer/DirectX/Shaders/_CompileShaders.bat
+++ Amethyst-Installer/DirectX/Shaders/_CompileShaders.bat
@@ -1,6 +1,6 @@
 @echo off
-call "%~dp0..\..\..\fxc.bat" /Tvs_4_0_level_9_1 /Evert /Fosimple_vert.cso simple.hlsl
-call "%~dp0..\..\..\fxc.bat" /Tps_4_0_level_9_1 /Efrag /Fosimple_frag.cso simple.hlsl
+call "%~dp0..\..\..\fxc.bat" /Tvs_5_0 /Evert /Fosimple_vert.cso simple.hlsl
+call "%~dp0..\..\..\fxc.bat" /Tps_5_0 /Efrag /Fosimple_frag.cso simple.hlsl
 
 REM Move to destination
 if not exist "%~dp0..\..\Resources\Shaders" mkdir "%~dp0..\..\Resources\Shaders"
