8a540cf73fd60887b47a92df73ca2f4eea0402be
HimbeersaftLP <mail@himbeer.me>
2023-04-18 11:59:23 +0200

    Use gotext-json for translations

---
+++ Amethyst-Installer/Util/Localisation/LocalisationFileJSON.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace amethyst_installer_gui {
+    public class LocalisationFileJSON {
+        [JsonProperty("messages")]
+        public List<LocalisedMessage> Messages;
+    }
+
+    public class LocalisedMessage {
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("translation")]
+        public string Translation;
+
+        [JsonProperty("comment")]
+        public string Comment;
+    }
+}
